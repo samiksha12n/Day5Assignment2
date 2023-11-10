fb92daa95db1cc3c7d6158a3dc3cfc9509160e92
samiksha12n <samikshaunesarkar@gmail.com>
2023-11-10 15:30:23 +0530

    Jagged Array

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5JaggedArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList StudentGrades = new ArrayList
+            {
+                85,92,78,95,88 
+            };
+            Console.WriteLine("Sum of Array Elemts:"+StudentGrades.Count);
+            int res = 0;
+            res= StudentGrades.Count;
+            Console.WriteLine(res);
+            foreach(int i in StudentGrades)
+            {
+                Console.WriteLine(i);
+            }
+            StudentGrades.Add(90);
+            Console.WriteLine("Added the new Grade");
+            foreach (int i in StudentGrades)
+            {
+                Console.WriteLine(i);
+            }
+            StudentGrades.RemoveAt(2);
+            Console.WriteLine("After removing ");
+            foreach (int i in StudentGrades)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
